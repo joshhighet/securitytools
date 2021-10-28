dbb0efce2a09e375734d40f41c6a5f27e06fd856
josh <github@joshhighet.com>
2021-10-28 13:30:18 +1300

    intro gh auth (combatpublic ratelimit)

--- report.py
+++ report.py
@@ -3,12 +3,20 @@ import logging
 import subprocess
 import requests
 
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if os.path.basename(os.getcwd()) != 'securitytools':
     print('must run from the securitytools repo!')
     exit()
+
+if len(os.sys.argv) != 2:
+    print('usage: report.py ${github_pat}')
+    exit()
+
+github_headers = {
+    'Authorization': 'token ' + os.sys.argv[1],
+    'Accept': 'application/vnd.github.v3+json'
+    }
 
 homedir = os.getcwd()
 with open(homedir + '/REPORT.md', 'w') as f:
@@ -25,7 +33,7 @@ with open(homedir + '/REPORT.md', 'w') as f:
                 os.chdir(directory)
                 # get the url of the submodule with git config --get remote.origin.url
                 url = subprocess.Popen(['git', 'config', '--get', 'remote.origin.url'], stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip().replace('.git', '')
-                ghapi = requests.get(url.replace('github.com', 'api.github.com/repos'), headers={'accept': 'application/vnd.github.v3+json'})
+                ghapi = requests.get(url.replace('github.com', 'api.github.com/repos'), headers=github_headers)
                 if ghapi.status_code != 200:
                     logging.error('could not get description for %s' % ghapi.url)
                     logging.error(ghapi.status_code + ' - ' + ghapi.text)
@@ -35,8 +43,9 @@ with open(homedir + '/REPORT.md', 'w') as f:
                     description = str(ghapi.json()['description'])
                 except KeyError:
                     logging.error(ghapi.text)
-                    description = 'no description exists'
-                f.write('\t_%s_\n' % description.strip())
+                    description = str()
+                if description != 'None':
+                    f.write('\t_%s_\n' % description.strip())
                 f.write('\n')
         os.chdir(homedir)
 f.close()
