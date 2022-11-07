ad2da6087e914febb20e79f36b4a41a250fe95a3
josh! <github@joshhighet.com>
2022-11-08 09:33:58 +1300

    sync

--- docs/skript.js
+++ docs/skript.js
@@ -4,48 +4,18 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
     var data = JSON.parse(xhr.responseText);
     var table = document.createElement('table');
-    var tr = document.createElement('tr');
-    var th = document.createElement('th');
-    th.innerHTML = 'path';
-    tr.appendChild(th);
-    th = document.createElement('th');
-    th.innerHTML = 'url';
-    tr.appendChild(th);
-    th = document.createElement('th');
-    th.innerHTML = 'description';
-    tr.appendChild(th);
-    th = document.createElement('th');
-    th.innerHTML = 'watchers';
-    tr.appendChild(th);
-    th = document.createElement('th');
-    th.innerHTML = 'forks';
-    tr.appendChild(th);
-    th = document.createElement('th');
-    th.innerHTML = 'stars';
-    tr.appendChild(th);
-    table.appendChild(tr);
-    for (var i = 0; i < data.length; i++) {
-      tr = document.createElement('tr');
-      var td = document.createElement('td');
-      td.innerHTML = data[i].path;
-      tr.appendChild(td);
-      td = document.createElement('td');
-      td.innerHTML = data[i].url;
-      tr.appendChild(td);
-      td = document.createElement('td');
-      td.innerHTML = data[i].description;
-      tr.appendChild(td);
-      td = document.createElement('td');
-      td.innerHTML = data[i].watchers;
-      tr.appendChild(td);
-      td = document.createElement('td');
-      td.innerHTML = data[i].forks;
-      tr.appendChild(td);
-      td = document.createElement('td');
-      td.innerHTML = data[i].stars;
-      tr.appendChild(td);
-      table.appendChild(tr);
-    }
+    var tableBody = document.createElement('tbody');
+    data.forEach(function(rowData) {
+      var row = document.createElement('tr');
+      Object.keys(rowData).forEach(function(key) {
+        var cell = document.createElement('td');
+        var cellText = document.createTextNode(rowData[key]);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      });
+      tableBody.appendChild(row);
+    });
+    table.appendChild(tableBody);
     document.body.appendChild(table);
   }
 }
